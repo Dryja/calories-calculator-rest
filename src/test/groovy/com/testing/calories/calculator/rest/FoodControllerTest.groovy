7d4b2834867c5e54553e305673258f9c6bb70e8a
kubehe <jakub.k.b@hotmail.com>
2018-12-09 12:44:06 +0100

    remove and update not tested todo: add UserFoodHistoryControllerTest

--- src/test/groovy/com/testing/calories/calculator/rest/FoodControllerTest.groovy
+++ src/test/groovy/com/testing/calories/calculator/rest/FoodControllerTest.groovy
@@ -1,29 +1,111 @@
 package com.testing.calories.calculator.rest
 
-import groovy.json.JsonSlurper
-
-import org.springframework.test.web.servlet.MockMvc
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import static org.springframework.http.HttpStatus.*
-
+import com.testing.calories.calculator.dto.FoodDTO
+import com.testing.calories.calculator.service.FoodService
 import org.springframework.beans.factory.annotation.Autowired
-
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
+import spock.mock.DetachedMockFactory
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(controllers = FoodController.class)
 class FoodControllerTest extends Specification {
+
+    @Autowired
+    MockMvc mvc
+
+    @Autowired
+    FoodService foodService
+
     def "GetFood"() {
+        given:
+        foodService.getFood("python") >> FoodDTO.builder().name("python").calories(100L).userFoodHistoryList([]).build()
+
+        expect: "controller returns UserDTO"
+        mvc.perform(get("/food/{name}", "python"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                "{\n" +
+                        "  \"name\": \"python\",\n" +
+                        "  \"calories\": 100,\n" +
+                        "  \"userFoodHistoryList\": []\n" +
+                        "}"))
     }
 
     def "GetFoodList"() {
+        given:
+        foodService.getFoodList() >> [
+                FoodDTO.builder().name("python").calories(100L).userFoodHistoryList([]).build(),
+                FoodDTO.builder().name("java").calories(99999999L).userFoodHistoryList([]).build()
+        ]
+
+        expect: "controller returns list of FoodDTO properly"
+        mvc.perform(get("/food"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                "[\n" +
+                        "  {\n" +
+                        "    \"name\": \"python\",\n" +
+                        "    \"calories\": 100,\n" +
+                        "    \"userFoodHistoryList\": []\n" +
+                        "  },\n" +
+                        "  {\n" +
+                        "    \"name\": \"java\",\n" +
+                        "    \"calories\": 99999999,\n" +
+                        "    \"userFoodHistoryList\": []\n" +
+                        "  }\n" +
+                        "]"
+        ))
     }
 
     def "AddFood"() {
+        given:
+        foodService.addFood(FoodDTO.builder()
+                .name("python")
+                .calories(100L).build()) >>
+                FoodDTO.builder()
+                        .name("python")
+                        .calories(100L).build()
+
+        expect: "controller returns newly created FoodDTO properly"
+        mvc.perform(post("/food").contentType(MediaType.APPLICATION_JSON).content(
+                "{\n" +
+                        "  \"calories\": 100,\n" +
+                        "  \"name\": \"python\"\n" +
+                        "}"
+        ))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(
+                "{\n" +
+                        "  \"name\": \"python\",\n" +
+                        "  \"calories\": 100,\n" +
+                        "  \"userFoodHistoryList\": null\n" +
+                        "}"
+        ))
     }
 
     def "RemoveFood"() {
     }
 
     def "UpdateFood"() {
+    }
+
+
+    @TestConfiguration
+    static class MockConfig {
+        def detachedMockFactory = new DetachedMockFactory()
+
+        @Bean
+        FoodService userService() {
+            return detachedMockFactory.Stub(FoodService)
+        }
     }
 }
--- src/test/groovy/com/testing/calories/calculator/rest/UserControllerTest.groovy
+++ src/test/groovy/com/testing/calories/calculator/rest/UserControllerTest.groovy
@@ -1,20 +1,106 @@
 package com.testing.calories.calculator.rest
 
+import com.testing.calories.calculator.dto.UserDTO
+import com.testing.calories.calculator.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import spock.lang.Specification
+import spock.mock.DetachedMockFactory
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(controllers = UserController.class)
 class UserControllerTest extends Specification {
+    @Autowired
+    MockMvc mvc
+
+    @Autowired
+    UserService userService
+
+
     def "GetUser"() {
+        given:
+        userService.getUser("test@test.pl") >> UserDTO.builder().email("test@test.pl").goal("test").userFoodHistoryList([]).build()
+
+        expect: "controller returns UserDTO"
+        mvc.perform(MockMvcRequestBuilders.get("/user/{email}", "test@test.pl"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                "{\n" +
+                        "  \"email\": \"test@test.pl\",\n" +
+                        "  \"goal\": \"test\",\n" +
+                        "  \"userFoodHistoryList\": []\n" +
+                        "}"))
     }
 
     def "GetUserList"() {
+        given:
+        userService.getUserList() >> [
+                UserDTO.builder().email("mateusz@podlasie.pl").goal("python").userFoodHistoryList([]).build(),
+                UserDTO.builder().email("kuba@mazowsze.pl").goal("javascript").userFoodHistoryList([]).build()
+        ]
+
+        expect: "controller returns list of UserDTO properly"
+        mvc.perform(MockMvcRequestBuilders.get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                "[\n" +
+                        "  {\n" +
+                        "    \"email\": \"mateusz@podlasie.pl\",\n" +
+                        "    \"goal\": \"python\",\n" +
+                        "    \"userFoodHistoryList\": []\n" +
+                        "  },\n" +
+                        "  {\n" +
+                        "    \"email\": \"kuba@mazowsze.pl\",\n" +
+                        "    \"goal\": \"javascript\",\n" +
+                        "    \"userFoodHistoryList\": []\n" +
+                        "  }\n" +
+                        "]"
+        ))
     }
 
     def "AddUser"() {
+        given:
+        userService.addUser(UserDTO.builder()
+                .email("python@python.pl")
+                .goal("python niszczy jave").build()) >>
+                UserDTO.builder()
+                        .email("python@python.pl")
+                        .goal("python niszczy jave").build()
+
+        expect: "controller returns newly created UserDTO properly"
+        mvc.perform(MockMvcRequestBuilders.post("/user").contentType(MediaType.APPLICATION_JSON).content(
+                "{ \"email\":\"python@python.pl\", \"goal\": \"python niszczy jave\"}"
+        ))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(
+                "{\n" +
+                        "  \"email\": \"python@python.pl\",\n" +
+                        "  \"goal\": \"python niszczy jave\",\n" +
+                        "  \"userFoodHistoryList\": null\n" +
+                        "}"
+        ))
     }
 
     def "RemoveUser"() {
     }
 
     def "UpdateUser"() {
+    }
+
+    @TestConfiguration
+    static class MockConfig {
+        def detachedMockFactory = new DetachedMockFactory()
+
+        @Bean
+        UserService userService() {
+            return detachedMockFactory.Stub(UserService)
+        }
     }
 }
