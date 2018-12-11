8e86bebde4a7d61535ddb0621ea8f088d97f8291
kubehe <jakub.k.b@hotmail.com>
2018-12-11 01:03:38 +0100

    copy paste xD

--- src/test/groovy/com/testing/calories/calculator/service/UserServiceTest.groovy
+++ src/test/groovy/com/testing/calories/calculator/service/UserServiceTest.groovy
@@ -1,5 +1,6 @@
 package com.testing.calories.calculator.service
 
+import com.testing.calories.calculator.dto.UserDTO
 import com.testing.calories.calculator.model.UserDetailsEntity
 import com.testing.calories.calculator.model.UserEntity
 import com.testing.calories.calculator.repository.UserDetailsRepository
@@ -15,6 +16,93 @@ class UserServiceTest extends Specification {
 
     def setup() {
         userService = new UserService(userRepository, userDetailsRepository)
+    }
+
+    def "AddUser"() {
+        given:
+        def userDto = UserDTO.builder().email(email).goal(goal).build()
+        def userEntity = UserEntity.builder().email(email).goal(goal).build()//UserMapper.toEntity(userDto)
+
+        and:
+        userRepository.findUserEntityByEmail(email) >> userEntity
+        userRepository.save(userEntity) >> userEntity
+
+        when:
+        def result = userService.addUser(userDto)
+
+        then:
+        assert result.goal == goal
+        assert result.email == email
+
+        where:
+        email            | goal
+        "ds@sa.pl"       | "goal"
+        "asdfa@asdaf.pl" | "goal"
+    }
+
+    def "RemoveUser"() {
+        given:
+        def userEntity = UserEntity.builder().goal(goal).email(email).build()
+
+        and:
+        userRepository.findUserEntityByEmail(email) >> userEntity
+
+        and:
+        userRepository.delete(userEntity) >> {}
+
+        when:
+        def result = userService.removeUser(email)
+
+        then:
+        assert result.goal == goal
+        assert result.email == email
+
+        where:
+        email              | goal
+        "nge@asdf.pl"      | "goal"
+        "lonadsfa@asdf.pl" | "goal"
+    }
+
+    def "GetUserList"() {
+        given:
+        def userList = [
+                UserEntity.builder().email("test@asd.pl").goal("goal").build(),
+                UserEntity.builder().email("test@asdf.lk").goal("goal").build(),
+                UserEntity.builder().email("testsdaf@sdaf.pl").goal("goal").build(),
+                UserEntity.builder().email("test3asdf@sad.pl").goal("goal").build(),
+                UserEntity.builder().email("test4@sa.pl").goal("goal").build(),
+        ]
+
+        and:
+        userRepository.findAll() >> userList
+
+        when:
+        def result = userService.getUserList()
+
+        then:
+        assert result.size() == userList.size()
+        assert result.get(0).email == userList.get(0).email
+
+    }
+
+    def "GetUser"() {
+        given:
+        def userEntity = UserEntity.builder().goal(goal).email(email).build()
+
+        and:
+        userRepository.findUserEntityByEmail(email) >> userEntity
+
+        when:
+        def result = userService.getUser(email)
+
+        then:
+        assert result.email == email
+        assert result.goal == goal
+
+        where:
+        email        | goal
+        "orange"     | "goal"
+        "watermelon" | "goal"
     }
 
     def "GetEnergyRequirements"() {
